6df7fa6b4445c894a28fea3073b029fd420400f4
carlos de jesus gomez arzola <gomezc523@hotmail.com>
2017-05-11 18:42:44 -0500

    calculadora con js
    se realizo calculadora con javascript y html

---
+++ js/java.js
@@ -0,0 +1,32 @@
+ function apachurrarS(){
+  
+    var num1 = Number(document.getElementById('N1').value)
+     var num2 = Number(document.getElementById('N2').value)
+    var sum=num1+num2;
+     var expresion;
+     
+      alert("El resultado de tu operacion es = "+(num1+num2)); 
+ }
+function apachurrarR(){
+  
+     var num1 = Number(document.getElementById('N1').value)
+     var num2 = Number(document.getElementById('N2').value)
+          alert("El resultado de tu operacion es = "+(num1-num2)); 
+    
+ }
+function apachurrarM(){
+  
+      var num1 = Number(document.getElementById('N1').value)
+     var num2 = Number(document.getElementById('N2').value)
+   
+          alert("El resultado de tu operacion es = "+(num1*num2)); 
+    
+ }
+function apachurrarD(){
+  
+     var num1 = Number(document.getElementById('N1').value)
+     var num2 = Number(document.getElementById('N2').value)
+   
+          alert("El resultado de tu operacion es = "+(num1/num2)); 
+
+ }
